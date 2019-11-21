aced7ea2adf586951123908603032ce39f26b497
Samuel Lundgren <samuell.person@gmail.com>
2019-11-21 16:17:56 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import Link from "next/link";
 import Head from "next/head";
 
 const Home = () => {
+  // uncomment to display errors in build logs
+  //   throw Error("An error");
+
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
       <h1>Home</h1>
-      <p>this is the homepage</p>
+      <p>this is the homepage!</p>
       <Link href="/about">
         <a>Go to about</a>
       </Link>
