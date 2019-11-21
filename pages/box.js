3edcc0c3e4f65b4119b780d31798bffd78f9d3f5
Samuel Lundgren <samuell.person@gmail.com>
2019-11-21 16:57:00 +0100

    setup

---
+++ pages/box.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Box = () => {
+  return (
+    <div>
+      <h1>Cat box</h1>
+      <Link href="/">
+        <a style={{ marginRight: "1rem" }}>Go to home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Box;
